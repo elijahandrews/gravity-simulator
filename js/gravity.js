92804d6fb371087aae1e8585fd59f1c258d14dec
Elijah Andrews <elijahcandrews@gmail.com>
2014-02-22 18:37:04 -0800

    Add collisions

--- js/gravity.js
+++ js/gravity.js
@@ -9,6 +9,7 @@ var G = 10;
 
 function Planet(radius, mass, x, y, color, vel_x, vel_y) {
   this.radius = radius;
+  this.orig_radius = radius;
   this.mass = mass;
   this.x = x;
   this.orig_x = x;
@@ -19,10 +20,13 @@ function Planet(radius, mass, x, y, color, vel_x, vel_y) {
   this.orig_vel_x = vel_x;
   this.vel_y = vel_y;
   this.orig_vel_y = vel_y;
+  this.killed = false;
 }
 
 Planet.prototype.reset = function() {
   this.clear();
+  this.killed = false;
+  this.radius = this.orig_radius;
   this.x = this.orig_x;
   this.y = this.orig_y;
   this.vel_x = this.orig_vel_x;
@@ -48,16 +52,26 @@ Planet.prototype.clear = function() {
 };
 
 Planet.prototype.move = function(modifier) {
+  if (this.killed) {
+    return
+  }
   this.clear();
   this.x += this.vel_x * modifier;
   this.y += this.vel_y * modifier;
   this.draw();
 };
 
+Planet.prototype.kill = function() {
+  this.clear();
+  this.killed = true;
+}
+
 var planets = new Array();
-planets.push(new Planet(20, 1500, 500, 300, 'green', 0, 0));
+planets.push(new Planet(20, 150, 500, 300, 'green', -30, -30));
 planets.push(new Planet(5, 30, 30, 100, 'blue', 110, -10));
 planets.push(new Planet(5, 30, 800, 700, 'red', 110, -10));
+planets.push(new Planet(30, 200, 200, 400, 'red', 110, -10));
+planets.push(new Planet(30, 200, 304, 700, 'purple', 20, -10));
 
 var reset = function() {
   planets.forEach(function(p) {
@@ -81,7 +95,7 @@ var stop = function() {
 var move = function (modifier) {
   planets.forEach(function(p1) {
     planets.forEach(function(p2) {
-      if (p1 == p2) {
+      if (p1 == p2 || p1.killed || p2.killed) {
         return;
       }
 
@@ -92,6 +106,26 @@ var move = function (modifier) {
       del_x = Math.abs(p2.x - p1.x);
       del_y = Math.abs(p2.y - p1.y);
       distance = Math.sqrt(Math.pow(del_x, 2) + Math.pow(del_y, 2));
+
+      // TODO: Handle collisiosn with multiple planets
+      if (distance <= p1.radius + p2.radius) {
+        if (p1.radius >= p2.radius) {
+          larger = p1;
+          smaller = p2;
+        } else {
+          larger = p2;
+          smaller = p1;
+        }
+
+        smaller.kill();
+        larger.radius *= 1 + smaller.radius/larger.radius;
+        larger.mass += smaller.mass;
+
+        larger.vel_x += smaller.vel_x * smaller.mass / larger.mass;
+        larger.vel_y += smaller.vel_y * smaller.mass / larger.mass;
+        // TODO: Combine their colours
+      }
+
       accel = G * p2.mass/Math.pow(distance, 2);
       theta = Math.atan(del_y/del_x);
       accel_x = accel * Math.cos(theta);
@@ -101,15 +135,13 @@ var move = function (modifier) {
     });
   });
 
-  // TODO: Check for collisions
-
   planets.forEach(function(p) {
     p.move(modifier);
   });
 }
 
 var then = Date.now();
-// The main game loop
+// The main loop
 var main = function () {
   var now = Date.now();
   var delta = now - then;
